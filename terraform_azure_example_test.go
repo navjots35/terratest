1048216f6674aca58d4f4fb579cfb8d11ce56f2b
Gaurav Singh <gauravkaransingh@gmail.com>
2021-06-01 12:04:48 +0530

    Updated go file

--- terraform_azure_example_test.go
+++ terraform_azure_example_test.go
@@ -22,7 +22,7 @@ func TestTerraformAzureExample(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/azure/terraform-azure-example",
+		TerraformDir: "../../Terratest/sample/terraform-azure-example",
 		Vars: map[string]interface{}{
 			"postfix": uniquePostfix,
 		},
